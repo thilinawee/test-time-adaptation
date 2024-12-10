8e5743b4d2daf35672d4370525bd31ad8758d37d
thilinawee <thilinaholmes@gmail.com>
2024-12-10 15:28:27 +1100

    Changing configs for oversampling imagenet_c TENT and EATA run scripts

--- classification/run_scripts/imagenet_c/dogs_eata_s5.sh
+++ classification/run_scripts/imagenet_c/dogs_eata_s5.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES=2
 
 
 python test_time.py --cfg cfgs/imagenet_c/eata.yaml \
-                         PRINT_EVERY 25 \
-                         TRAIN_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet/DOGS_EXTENDED_S5 \
+                         PRINT_EVERY 50 \
+                         TRAIN_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet \
                          TEST_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet \
                          PROJECT_NAME imagenet_c_test-time-adaptation \
-                         RUN_NAME dogs_eata_s5
+                         RUN_NAME dogs_eata_s5_weighted_random_oversample
--- classification/run_scripts/imagenet_c/dogs_tent_s5.sh
+++ classification/run_scripts/imagenet_c/dogs_tent_s5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 
 
 python test_time.py --cfg cfgs/imagenet_c/tent.yaml \
-                         PRINT_EVERY 25 \
-                         TRAIN_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet/DOGS_EXTENDED_S5 \
+                         PRINT_EVERY 50 \
+                         TRAIN_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet \
                          TEST_DATA_DIR /home/thilina/SSD2/thilina/datasets/imagenet \
                          PROJECT_NAME imagenet_c_test-time-adaptation \
-                         RUN_NAME dogs_tent_s5
+                         RUN_NAME dogs_tent_s5_weighted_random_oversample
